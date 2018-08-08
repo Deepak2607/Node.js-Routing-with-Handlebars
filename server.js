9da401c58d0db4a54734efe15dcfb721d2804381
Deepak Kumrawat <deepakkumrawat8@gmail.com>
2018-08-08 19:28:48 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,40 @@
+const express= require('express');
+const hbs= require('hbs');
+
+const app=express();
+
+
+app.use('/public',express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        title:'About Page',
+        year:new Date().getFullYear()
+    });
+});
+
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        title:'Home Page',
+        year:new Date().getFullYear(),
+        message:'Welcome to my Website'
+    });
+});
+
+
+//app.get('/info',(req,res)=>{
+//    res.send({
+//        name:'Deepak Kumrawat',
+//        address:{
+//            college:'ABV-IIITM Gwalior',
+//            location:'Morena Link Road',
+//        }
+//    });
+//});
+
+app.listen(8000, ()=> {
+    console.log('Local server started on port 8000!')
+});
